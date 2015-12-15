aee55d650dcda749f7a769f8bf43b013d4df60f4
Esteban Cairol <ecairol@gmail.com>
2015-12-14 22:43:47 -0600

    Moved to a Linear SVC classifier. Removed Neutral reviews from data processing

--- machine.py
+++ machine.py
@@ -1,26 +1,28 @@
 """
 Author: Esteban Cairol
-Text classifier.
+Text classifier. Sentiment analysis from Amazon reviews
 """
 import sys
 import os
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn import svm
+
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
 
 # from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import load_files
-from sklearn import metrics
+# from sklearn.datasets import load_files
+
 
 path = 'data'
 files = os.listdir(path)
-perc_train = 80
+perc_train = 0.8
 
 # Randomize files
 random.shuffle(files)
@@ -30,6 +32,9 @@ labels = []
 
 for file_name in files:
     if file_name.endswith(".txt"):
+    	# Ignore reviews with 3 stars ('Neutral' reviews, starting with 1)
+    	if file_name.startswith("1"):
+    		continue
     	test = file_name
     	file = open(os.path.join(path, file_name) ,'r')
     	samples.append(file.read())
@@ -37,22 +42,50 @@ for file_name in files:
     	file.close()
 
 n_samples = len(samples)
+index_split = int(perc_train*n_samples)
+
 print "Total samples read: {}".format(n_samples)
 
-train_samples = samples[0:(perc_train*n_samples)]
-train_labels = labels[0:(perc_train*n_samples)]
+train_samples = samples[0:index_split]
+train_labels = labels[0:index_split]
 
 perc_test = 100-perc_train
-test_samples = samples[0:(perc_test*n_samples)]
-test_labels = labels[0:(perc_test*n_samples)]
+test_samples = samples[index_split+1:n_samples]
+test_labels = labels[index_split+1:n_samples]
 
 text_clf = Pipeline([
-	('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MultinomialNB()), 
+	('vect', TfidfVectorizer(max_df = 0.8,sublinear_tf=True,use_idf=True)),
+	('clf', svm.LinearSVC(C=1.0)),
 ])
 
+
+print "Starting fit process..."
 text_clf.fit(train_samples, train_labels)
 
+print "Starting prediction..."
 predicted = text_clf.predict(test_samples)
-accuracy = text_clf.score(test_samples, test_labels)
 
+
+# Calculate Accuracy
+print "Calculating accuracy..."
+accuracy = text_clf.score(test_samples, test_labels)
 print "Accuracy {}".format(accuracy)
+
+# Confusion Matrix Analysis
+print "Building confusion matrix..."
+conf = confusion_matrix(test_labels, predicted)
+print "Confusion Matrix \n {}".format(conf)
+plt.imshow(conf, cmap='binary', interpolation='None')
+
+
+"""
+Developer Notes
+------------------------------------------------------------------------------------
+ - The original idea was to use not only Positive and Negative labels, but also Neutral label representing 3-star reviews. However I learned that this increases 
+   the noise and confusion, so I decided to keep only 2 classes: positive (2) or negative (0)
+ - I first started using MultinomialNB() classifier, but svm.LinearSVC() gives a better accuracy at a similar cost
+ - svm.LinearSVC() process data faster than svm.SVC(kernel='linear')
+ - On TfidfVectorizer(), max_df = 0.8 words appering in more than 80% of the documents
+ - TfidfVectorizer() is being used as both, Vectorizes and Transformer (instead of CountVectorizer and then TfidfTransformer)
+------------------------------------------------------------------------------------
+"""
